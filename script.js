30ed712fd85f917f0101efdfcc344e3ab6fea5bc
pedroleonardo945 <pedro.stanczak.oliveira@escola.pr.gov.br>
2025-10-21 08:07:08 -0300

    primeira coisas

---
+++ script.js
@@ -0,0 +1,16 @@
+const botaoMostraPalavras = document.querySelector('#botao-palavrachave');
+
+botaoMostraPalavras.addEventListener('click', mostraPalavrasChave);
+
+function mostraPalavrasChave() {
+    const texto = document.querySelector('#entrada-de-texto').value;
+    const campoResultado = document.querySelector('#resultado-palavrachave');
+    const palavrasChave = processaTexto(texto);
+
+    campoResultado.textContent = palavrasChave.join(", ");
+}
+
+function processaTexto(texto){
+    let palavras = texto.split(/[^a-zA-Z]+/);
+    return palavras;
+}
